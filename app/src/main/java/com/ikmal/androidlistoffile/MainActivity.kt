c962f9cb8d3c1ddd1af472bf1379e140a36f379b
Januar Ikmal <ikmal.dev@gmail.com>
2022-01-27 07:46:55 +0700

    Adding viewbinding

--- app/src/main/java/com/ikmal/androidlistoffile/MainActivity.kt
+++ app/src/main/java/com/ikmal/androidlistoffile/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.ikmal.androidlistoffile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ikmal.androidlistoffile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
